d8ef5d4edb27d1ece2201548d208355bfebc8be1
Fazouane Marouane <fazouanem3@gmail.com>
2017-02-11 17:19:46 +0100

    get computed style

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,35 @@
 console.log('hey')
+var canvas = document.createElement('canvas');
+var ctx = <CanvasRenderingContext2D>canvas.getContext('2d');
+
+function computeWidth(height: number, text: string) {
+  ctx.font = height + "px 'Source Sans Pro',Arial,sans-serif";
+  return ctx.measureText(text).width;
+}
+
+interface FontInfo {
+  font: string;
+  size: number;
+  sizeUnit: string;
+}
+
+function fontInfo(element: HTMLElement): FontInfo {
+  let computedStyle = window.getComputedStyle(element);
+  let fontSizeDetails = computedStyle.fontSize!.match(/(\d+)(\w+)/);
+  return {
+    font: <string>computedStyle.fontFamily,
+    size: Number(fontSizeDetails![1]),
+    sizeUnit: fontSizeDetails![2]
+  };
+}
+
+export function fillParentContainer(element: HTMLElement, opts: any): void {
+  let { font, size: initialSize, sizeUnit } = fontInfo(element);
+  var text = element.textContent;
+  // ...
+  let finalSize = initialSize;
+  element.style.fontSize = finalSize + sizeUnit;
+}
 
 export function dummy() {
   return 42;
