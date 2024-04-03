afc714f9ba7e7a47fbb6eef25a44fa1d67dc8d1b
Marouane Fazouane <fazouanem3@gmail.com>
2024-04-03 13:03:46 +0200

    Faster & simpler lru implementation

--- src/lru/index.ts
+++ src/lru/index.ts
@@ -1,37 +1,12 @@
-/**
- * @todo put this in a separate package
- */
-export type NullableCacheEntry<TKey, TValue> = CacheEntry<TKey, TValue> | null;
-
-export class CacheEntry<TKey, TValue> {
-	older: NullableCacheEntry<TKey, TValue> = null;
-	newer: NullableCacheEntry<TKey, TValue> = null;
-	key: TKey;
-	value: TValue;
-	constructor(key: TKey, value: TValue) {
-		this.key = key;
-		this.value = value;
-	}
-}
-
 export class LRUCache<TKey extends string | number, TValue> {
-	/* double linked list */
-	size = 0;
-	oldest: NullableCacheEntry<TKey, TValue> = null;
-	newest: NullableCacheEntry<TKey, TValue> = null;
 	limit = 0;
-	private keymap: Map<TKey, CacheEntry<TKey, TValue>> = new Map<
-		TKey,
-		CacheEntry<TKey, TValue>
-	>();
+	private keymap: Map<TKey, TValue> = new Map<TKey, TValue>();
 
 	constructor(limit = 0) {
 		this.limit = limit;
 	}
 
 	clear() {
-		this.oldest = this.newest = null;
-		this.size = 0;
 		this.keymap.clear();
 	}
 
@@ -39,48 +14,26 @@ export class LRUCache<TKey extends string | number, TValue> {
 		if (!this.keymap.has(key)) {
 			return undefined;
 		}
-		const entry = this.keymap.get(key)!;
-		if (this.newest !== entry) {
-			const older = entry.older;
-			const newer = entry.newer;
-			entry.newer = null;
-			entry.older = this.newest;
-			this.newest!.newer = entry;
-			this.newest = entry;
-			newer!.older = older;
-			if (older) {
-				older.newer = newer;
-			}
-			if (entry === this.oldest) {
-				this.oldest = newer;
-			}
-		}
-		return entry.value;
+		const value = this.keymap.get(key);
+		this.keymap.delete(key);
+		this.keymap.set(key, value!);
+		return value;
 	}
 
 	setItem(key: TKey, value: TValue) {
-		if (this.getItem(key)) {
-			const entry = this.keymap.get(key)!;
-			entry.value = value;
+		if (!this.keymap.has(key)) {
+			this.keymap.set(key, value);
+			if (this.limit > 0 && this.keymap.size > this.limit) {
+				const iterator = this.keymap.entries().next();
+				if (iterator.done) {
+					return;
+				}
+				const [oldestKey] = iterator.value;
+				this.keymap.delete(oldestKey);
+			}
 		} else {
-			const entry = new CacheEntry<TKey, TValue>(key, value);
-			if (!this.limit || this.size < this.limit) {
-				this.size++;
-			} else {
-				const firstEntry = this.oldest;
-				this.oldest = firstEntry!.newer;
-				this.oldest!.older = null;
-				this.keymap.delete(firstEntry!.key);
-			}
-			entry.older = this.newest;
-			if (this.newest) {
-				this.newest.newer = entry;
-			}
-			this.newest = entry;
-			if (!this.oldest) {
-				this.oldest = entry;
-			}
-			this.keymap.set(key, entry);
+			this.keymap.delete(key);
+			this.keymap.set(key, value);
 		}
 	}
 }
@@ -89,7 +42,7 @@ export function lru_cached(limit: number) {
 	const cache = new LRUCache<string, any>(limit);
 	return (target: any) =>
 		(...args: any[]) => {
-			const key = args.map((v) => `${v}`).join(";");
+			const key = JSON.stringify(args);
 			let value = cache.getItem(key);
 			if (!value) {
 				value = target.apply(null, args);
